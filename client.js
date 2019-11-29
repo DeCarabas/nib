704997ed07d97ef3f49018ae68ac5a2a95c4d3d0
John Doty <john@d0ty.me>
2019-11-29 06:23:19 -0800

    History Behavior

--- client.js
+++ client.js
@@ -3,24 +3,64 @@ const reactDom = require("react-dom");
 const storage = require("./storage");
 const wiki = require("./wiki");
 
-const { useState } = react;
+const { useEffect, useRef, useState } = react;
 const e = react.createElement;
 
 function ContentPage({ initialDocument, store }) {
-  const [root, setRoot] = useState("index");
+  const [history, setHistory] = useState(["index"]);
+  const cardRefs = useRef([]);
 
-  return e(Card, {
-    key: root,
-    slug: root,
-    store,
-    onNavigate: setRoot
+  useEffect(() => {
+    cardRefs.current = cardRefs.current.slice(0, history.length);
+    if (cardRefs.current) {
+      const focus = cardRefs.current[history.length - 1];
+      if (focus) {
+        const rect = focus.getBoundingClientRect();
+        window.scrollTo({
+          left: Math.max(0, rect.left - 10),
+          top: Math.max(0, rect.top - 10),
+          behavior: "smooth"
+        });
+      }
+    }
   });
+
+  // Rebuild the grid. As history becomes more sophisticated so will this.
+  const columns = history.map(_ => "auto").join(" ");
+  const cards = history.map((slug, i) =>
+    e(
+      "div",
+      {
+        key: i.toString(),
+        style: { gridColumnStart: i + 1 },
+        ref: el => (cardRefs.current[i] = el)
+      },
+      e(Card, {
+        slug,
+        store,
+        onNavigate: x => {
+          const newHistory = history.slice(0, i + 1);
+          newHistory.push(x);
+          setHistory(newHistory);
+        }
+      })
+    )
+  );
+
+  return e(
+    "div",
+    {
+      style: { display: "grid", gridTemplateColumns: columns },
+      overflow: "scroll"
+    },
+    cards
+  );
 }
 
 function Card({ slug, store, onNavigate }) {
   return e(
     "div",
-    { className: "ba pa3 ma2 mw6 relative" },
+    { className: "ba pa3 ma2 w6 relative" },
     e(wiki.WikiCard, { slug, store, onNavigate })
   );
 }
