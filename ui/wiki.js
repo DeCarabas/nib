fb97fb0a5f3f5cee89afcea6fb42b11237209d82
John Doty <john@d0ty.me>
2019-12-19 12:57:59 -0800

    Back to markdown-it.

--- ui/wiki.js
+++ ui/wiki.js
@@ -4,17 +4,111 @@
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./extern/preact.10.1.1/preact", "./extern/preact.10.1.1/hooks", "./extern/marked.0.8.0/marked", "./extern/codemirror.5.49.2/lib/codemirror", "./extern/codemirror.5.49.2/mode/markdown/markdown"], factory);
+        define(["require", "exports", "./extern/preact.10.1.1/preact", "./extern/preact.10.1.1/hooks", "./extern/markdown-it.10.0.0/markdown-it", "./extern/codemirror.5.49.2/lib/codemirror", "./extern/codemirror.5.49.2/mode/markdown/markdown"], factory);
     }
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     const preact_1 = require("./extern/preact.10.1.1/preact");
     const hooks_1 = require("./extern/preact.10.1.1/hooks");
-    const marked_1 = require("./extern/marked.0.8.0/marked");
+    const MarkdownIt = require("./extern/markdown-it.10.0.0/markdown-it");
     const CodeMirror = require("./extern/codemirror.5.49.2/lib/codemirror");
     require("./extern/codemirror.5.49.2/mode/markdown/markdown");
     const NIB_SCHEME = "nib://";
+    const md = new MarkdownIt();
+    // Handle wikimedia-style links.
+    function parseWikiLink(state, silent) {
+        const source = state.src;
+        let pos = state.pos;
+        let matched = false;
+        if (source[pos] === "[" && source[pos + 1] === "[") {
+            // scan forward until we find the close...
+            pos += 2;
+            while (pos < source.length) {
+                if (source[pos] === "]" && source[pos + 1] === "]") {
+                    matched = true;
+                    break;
+                }
+                // exit out early if we see something wierd
+                if (source[pos] === "[") {
+                    break;
+                }
+                pos += 1;
+            }
+        }
+        if (!matched) {
+            return false;
+        }
+        const start = state.pos + 2;
+        const end = pos;
+        if (!silent) {
+            const match = source.slice(start, end).trim();
+            const pipeIndex = match.indexOf("|");
+            const [target, text] = pipeIndex >= 0
+                ? [match.slice(0, pipeIndex).trim(), match.slice(pipeIndex + 1).trim()]
+                : [match, match];
+            const token = state.push("wiki-link", "a", 0);
+            token.meta = { target, text };
+        }
+        state.pos = end + 2;
+        return true;
+    }
+    md.inline.ruler.push("wikiLinks", parseWikiLink);
+    // Render wikimedia style links.
+    function renderWikiLinkHTML(tokens, id, _options, _env) {
+        const { target, text } = tokens[id].meta;
+        return '<a href="' + NIB_SCHEME + target + '">' + text + "</a>";
+    }
+    md.renderer.rules["wiki-link"] = renderWikiLinkHTML;
+    // Configure markdown renderer to add the right classes to stuff.
+    function processLink(token) {
+        token.attrPush(["target", "_blank"]); // Open in new window.
+        return token;
+    }
+    function addClass(token, className) {
+        const aIndex = token.attrIndex("class");
+        if (aIndex >= 0) {
+            token.attrs[aIndex][1] += " " + className;
+        }
+        else {
+            token.attrPush(["class", className]);
+        }
+        return token;
+    }
+    // Style text correctly with tachyons? This sucks kinda...
+    function processTokens(tokens) {
+        return tokens.map((token, index, tokens) => {
+            // console.log(token);
+            if (token.children) {
+                token.children = processTokens(token.children);
+            }
+            switch (token.type) {
+                case "link_open":
+                    return processLink(token);
+                case "heading_open":
+                    switch (token.tag) {
+                        case "h1":
+                            return addClass(token, "f2 lh-solid");
+                        case "h2":
+                            return addClass(token, "f3 lh-solid");
+                        case "h3":
+                            return addClass(token, "f4 lh-solid");
+                        case "h4":
+                            return addClass(token, "f5 lh-solid");
+                        case "h5":
+                        case "h6":
+                            return addClass(token, "f6 lh-solid");
+                    }
+                    return token;
+                case "paragraph_open":
+                    return addClass(token, "measure lh-copy");
+                default:
+                    return token;
+            }
+        });
+    }
+    const defaultRender = md.renderer.render.bind(md.renderer);
+    md.renderer.render = (tokens, options, env) => defaultRender(processTokens(tokens), options, env);
     function CodeMirrorEditor({ value, onChange }) {
         const ref = hooks_1.useRef(null);
         const codeMirror = hooks_1.useRef({ value, cm: null, elt: null });
@@ -75,11 +169,7 @@
     }
     exports.WikiEditor = WikiEditor;
     function MarkdownView({ markdown }) {
-        const options = {
-            headerIds: false,
-            silent: false
-        };
-        const html = marked_1.parser(marked_1.lexer(markdown, options), options);
+        const html = md.render(markdown);
         return preact_1.h("div", { dangerouslySetInnerHTML: { __html: html } });
     }
     function WikiView({ slug, document, onNavigate }) {
